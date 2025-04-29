9dce2f98a75ecd4648722ce6e27c0c27c33c2fa4
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-04-29 22:05:54 +0100

    Refactor routing and header components; streamline route definitions and update header links for improved navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-
+import { MenuComponent } from './pages/menu/menu.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
   { path: 'about', component: AboutComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'checkout', component: CheckoutComponent },
+
 ];

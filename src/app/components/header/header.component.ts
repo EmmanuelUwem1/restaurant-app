3ac769d8979fbbb6f424901cac458dd1174a9ebf
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-04-29 15:04:15 +0100

    Refactor app layout and enhance header component; add new components for home page and update styles

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
+  headerLinks = [
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' },
+    { name: 'Menu', path: '/menu' },
+    { name: 'Gallery', path: '/gallery' },
+    { name: 'Contact', path: '/contact' },
+  ];
 
+  isActive(route: string): boolean {
+    return this.router.url === route; // Returns true if the current route matches
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './components/hero/hero.component';
+import { FeaturedComponent } from './components/featured/featured.component';
+import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { NewsletterSectionComponent } from "./components/newsletter-section/newsletter-section.component";
+import { AboutChefComponent } from "./components/about-chef/about-chef.component";
+import { RestaurantVideoComponent } from "./components/restaurant-video/restaurant-video.component";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  imports: [HeroComponent, FeaturedComponent, AboutSectionComponent, NewsletterSectionComponent, AboutChefComponent, RestaurantVideoComponent],
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {}

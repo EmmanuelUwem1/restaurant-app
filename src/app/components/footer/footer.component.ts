f565455e6ed94a83e894fe0daf41a54909470824
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-04-29 21:57:18 +0100

    Enhance footer and restaurant video components; update layout and content for improved user experience

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+socialLinks = [
+    { name: 'Facebook', iconUrl: '/facebook.png' },
+    { name: 'Twitter', iconUrl: '/twitter.png' },
+    { name: 'Instagram', iconUrl: '/instagram.png' },
+    { name: 'Whatsapp', iconUrl: '/whatsapp.png' },
+]
 }

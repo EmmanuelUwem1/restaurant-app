6aaa432ce7c7f7be0be7ca12d411b36d1424b82a
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-04-30 21:45:17 +0100

    feat: update checkout and gallery components for improved layout and styling

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [RouterModule, FooterComponent],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
